2ec252d24b4a67b23ee84727919e530a526417ac
content Tingching0602 <contentanita2131love@gmail.com>
2022-07-27 17:18:23 +0800

    update

---
+++ src/views/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h2>各樣植物列表</h2>
+     <div id="menu" class="row">
+      <div class="col-3">
+       <div class="card">
+          <img src="../assets/image/plant2.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">凹突觀音蓮</h5>
+            <p class="card-text">1200</p>
+            <a href="#" class="btn btn-primary">加入購物車</a>
+          </div>
+        </div>
+      </div>
+     </div>
+    </div>
+</template>
