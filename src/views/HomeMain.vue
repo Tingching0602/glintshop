f6c62e35ed11e4edf136e6504e4e6dc73ef3d333
content Tingching0602 <contentanita2131love@gmail.com>
2022-07-31 20:10:07 +0800

    update

---
+++ src/views/HomeMain.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+<NavbarHome2 />
+  <dir class= 'cover'><img src="../assets/image/cover.png" alt="" ></dir>
+  </div>
+</template>
+<script>
+/*eslint-disable*/
+import NavbarHome2 from '../components/NavbarHome2.vue'
+export default {
+ components: {
+    NavbarHome2
+ }
+}
+</script>
